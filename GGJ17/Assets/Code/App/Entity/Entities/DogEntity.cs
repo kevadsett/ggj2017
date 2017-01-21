51d535a522091dee143d737190c20b5e7ace1e61
Kev Adsett <kev.adsett@gmail.com>
2017-01-21 15:03:23 +0000

    Add dog entity (WIP)

---
+++ GGJ17/Assets/Code/App/Entity/Entities/DogEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogEntity : Entity {
+	private GameObject _gameObject;
+	public DogEntity (int zId, int zX, int zZ) : base (zId, zX, zZ)
+	{
+		var gameData = GameDataBase.Instance.GetData ();
+		var prefab = gameData.SheepPrefab;
+	}
+
+	public void Update()
+	{
+		
+	}
+
+}
