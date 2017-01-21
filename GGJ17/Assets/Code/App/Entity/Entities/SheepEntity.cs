024de930ae5e4a29682151c0438939f0bd7fd225
gav <notoriousknave@gmail.com>
2017-01-21 13:29:53 +0000

    Made (empty) SheepEntity

---
+++ GGJ17/Assets/Code/App/Entity/Entities/SheepEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class SheepEntity : Entity
+{
+	private RollingCube viewCube;
+
+	public SheepEntity (int zId, int zX, int zZ) : base (zId, zX, zZ)
+	{
+		// create viewCube and position
+	}
+
+
+}
