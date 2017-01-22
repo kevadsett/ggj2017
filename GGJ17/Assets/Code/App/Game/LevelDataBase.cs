fc1ddaa7aa20be25ebbe1195b932f6ffa0948d22
Kev Adsett <kev.adsett@gmail.com>
2017-01-22 10:02:39 +0000

    Add concept of levels and level database.

---
+++ GGJ17/Assets/Code/App/Game/LevelDataBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelDataBase : MonoBehaviour {
+	public static LevelDataBase Instance;
+	public List<LevelData> Levels;
+
+	void Awake()
+	{
+		Instance = this;
+	}
+
+	public LevelData GetLevel(int index)
+	{
+		return Levels[index];
+	}
+}
