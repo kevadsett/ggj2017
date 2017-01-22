f338f50fa957f6ff561930634d1ba5d5ef0510a6
Le-Roy Karunaratne <lrkaru@gmail.com>
2017-01-22 13:12:12 +0000

    Reset eulogy position before displaying it

---
+++ GGJ17/Assets/Code/App/UI/ResetEulogy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetEulogy : MonoBehaviour
+{
+	private static ResetEulogy Instance;
+
+
+	private void Start()
+	{
+		Instance = this;
+	}
+
+	public static void ResetPosition()
+	{
+		RectTransform rect = Instance.transform as RectTransform;
+		rect.localPosition = new Vector3(0, -400, 0);
+	}
+}
