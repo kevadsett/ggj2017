5cdcb55ae6aa26cdc8682ca048942af86c64b47b
Le-Roy Karunaratne <lrkaru@gmail.com>
2017-01-22 01:38:26 +0000

    Started adding eulogy rendering

---
+++ GGJ17/Assets/Code/App/Eulogy/EulogyData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EulogyData
+{
+	public string Name;
+	public string Date;
+	public string Summary;
+}
