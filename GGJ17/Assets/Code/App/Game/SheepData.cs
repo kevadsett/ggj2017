fca19060784cc26acf70be1b3e00f4927da3cc57
Kev Adsett <kev.adsett@gmail.com>
2017-01-22 14:17:11 +0000

    Sheep Data

---
+++ GGJ17/Assets/Code/App/Game/SheepData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class SheepData : ScriptableObject {
+	private int _id;
+	public int ID { get { return _id; } }
+	public string Name;
+	public string Dates;
+	public string EulogyText;
+	public string FinalText;
+
+	public void SetID(int id)
+	{
+		_id = id;
+	}
+}
---
+++ GGJ17/Assets/Code/App/Game/SheepDataBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepDataBase : MonoBehaviour {
+	public static SheepDataBase Instance;
+	public List<SheepData> Sheep;
+
+	void Awake()
+	{
+		Instance = this;
+	}
+
+	public SheepData GetSheep(int id)
+	{
+		for (int i = 0; i < Sheep.Count; i++)
+		{
+			if (Sheep [i].ID == id)
+			{
+				return Sheep [i];
+			}
+		}
+		return null;
+	}
+}
