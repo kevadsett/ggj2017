9899bc7427d27c3723726129869db0c77f37ec74
Le-Roy Karunaratne <lrkaru@gmail.com>
2017-01-22 12:27:12 +0000

    started on zzzzzz

---
+++ GGJ17/Assets/Code/App/Effects/SleepParticle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SleepParticle : MonoBehaviour
+{
+	[SerializeField]
+	private float m_LifeTime = 1.5f;
+
+	[SerializeField]
+	private AnimationCurve m_Curve;
+
+
+	private float mStartTime;
+
+
+	private void Start()
+	{
+		mStartTime = Time.time;
+	}
+
+	private void Update()
+	{
+		float ratio = (Time.time - mStartTime) / m_LifeTime;
+
+		if (ratio >= 1f)
+		{
+			GameObject.Destroy(gameObject);
+		}
+
+		var newPosition = transform.position;
+
+
+
+		transform.position = newPosition;
+	}
+}
